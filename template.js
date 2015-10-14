48666f8608e843b787ef6316f043b8b6ca0ac76c
Alberto Donato <alberto.donato@gmail.com>
2015-10-15 00:04:39 +0300

    Use chart.js instead of matplotlib.

---
+++ template.js
@@ -0,0 +1,21 @@
+var plot_graph = function(graphData) {
+    var context = document.getElementById(graphData.id).getContext("2d");
+
+    return new Chart(context).Line({
+        labels: graphData.x,
+        datasets: [
+            {
+                label: graphData.label,
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: graphData.y
+            }]});
+};
+
+{% for graph in graphs %}
+x = plot_graph({{ graph | json }});
+{% endfor %}
