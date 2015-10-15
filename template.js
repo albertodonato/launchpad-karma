5f7a877149da2970dd5d0b03d991261ded457272
Alberto Donato <alberto.donato@gmail.com>
2015-10-15 14:56:17 +0300

    Use same scale for all graphs.

--- template.js
+++ template.js
@@ -19,21 +19,27 @@
 
 // Plot a char for specified data
 var plotChart = function(graphData) {
-    var context = document.getElementById(graphData.id).getContext("2d");
+    var context = document.getElementById(graphData.id).getContext("2d"),
+        data = {
+            labels: graphData.x,
+            datasets: [
+                {
+                    label: graphData.label,
+                    data: graphData.y,
+                    fillColor: 'rgba(151,187,205,0.2)',
+                    strokeColor: 'rgba(151,187,205,1)',
+                    pointColor: 'rgba(151,187,205,1)',
+                    pointStrokeColor: '#fff',
+                    pointHighlightFill: '#fff',
+                    pointHighlightStroke: 'rgba(151,187,205,1)'
+                }]},
+        options = {
+            scaleOverride: true,
+            scaleSteps : {{ graph_config.scale_steps }},
+            scaleStepWidth : {{ graph_config.scale_step_width }},
+            scaleStartValue : {{ graph_config.scale_start_value }}};
 
-    return new Chart(context).Line({
-        labels: graphData.x,
-        datasets: [
-            {
-                label: graphData.label,
-                data: graphData.y,
-                fillColor: 'rgba(151,187,205,0.2)',
-                strokeColor: 'rgba(151,187,205,1)',
-                pointColor: 'rgba(151,187,205,1)',
-                pointStrokeColor: '#fff',
-                pointHighlightFill: '#fff',
-                pointHighlightStroke: 'rgba(151,187,205,1)'
-            }]});
+    return new Chart(context).Line(data, options);
 };
 
 // Update graph defaults
