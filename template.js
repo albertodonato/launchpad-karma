8fa4cbefa22ad9f6d75215c199edd2d0343e97a0
Alberto Donato <alberto.donato@gmail.com>
2015-10-15 10:13:57 +0300

    Better styles.

--- template.js
+++ template.js
@@ -1,4 +1,24 @@
-var plot_graph = function(graphData) {
+//
+// Author: Alberto Donato
+//
+// This file is part of launchpad-karma.
+//
+// launchpad-karma is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// launchpad-karma is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with launchpad-karma.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+// Plot a char for specified data
+var plotChart = function(graphData) {
     var context = document.getElementById(graphData.id).getContext("2d");
 
     return new Chart(context).Line({
@@ -6,16 +26,24 @@ var plot_graph = function(graphData) {
         datasets: [
             {
                 label: graphData.label,
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: graphData.y
+                data: graphData.y,
+                fillColor: 'rgba(151,187,205,0.2)',
+                strokeColor: 'rgba(151,187,205,1)',
+                pointColor: 'rgba(151,187,205,1)',
+                pointStrokeColor: '#fff',
+                pointHighlightFill: '#fff',
+                pointHighlightStroke: 'rgba(151,187,205,1)'
             }]});
 };
 
+// Update graph defaults
+Chart.defaults.global.scaleFontFamily = 'Ubuntu';
+Chart.defaults.global.scaleFontStyle = 300;
+Chart.defaults.global.tooltipFontFamily = 'Ubuntu';
+Chart.defaults.global.tooltipFontStyle = 300;
+Chart.defaults.global.responsive = true;
+
+// Plot data
 {% for graph in graphs %}
-plot_graph({{ graph | json }});
+plotChart({{ graph | json }});
 {% endfor %}
